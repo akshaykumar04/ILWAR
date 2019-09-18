706868594aa0713c85e8b6c38663e02f787bdf24
mukultarania <taraniamukul@gmail.com>
2019-09-18 22:51:00 +0530

    Bottom NAv Done

--- app/src/main/java/com/am/minor/MainActivity.java
+++ app/src/main/java/com/am/minor/MainActivity.java
@@ -1,46 +1,58 @@
 package com.am.minor;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.am.minor.fragments.HomeFragment;
 import com.am.minor.fragments.ProfileFragment;
 import com.am.minor.fragments.TutorialFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    Button home, profile, tutorial;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bottomnav);
-//        home= findViewById(R.id.home);
-//        profile = findViewById(R.id.profile);
-//        tutorial = findViewById(R.id.tutorial);
+        setContentView(R.layout.bottom_nav);
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
     }
 
-    public void onClick(View view) {
-        Fragment fragment = new HomeFragment();
-        ;
-        if (view == findViewById(R.id.home)) {
-            fragment = new HomeFragment();
-
-        } else if (view == findViewById(R.id.profile)) {
-            fragment = new ProfileFragment();
-
-        } else if (view == findViewById(R.id.tutorial)) {
-            fragment = new TutorialFragment();
-
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.output, fragment)
+                    .commit();
+            return true;
         }
-        FragmentManager manager = getSupportFragmentManager();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.output, fragment);
-        transaction.commit();
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+
+            case R.id.navigation_tutorial:
+                fragment = new TutorialFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
     }
 }
