564bf061f107d7fc49c19431bad038047da0e516
Akshay Kumar <mail.akshay04@gmail.com>
2019-12-16 15:50:43 +0530

    BooksStore Fragment added

--- app/src/main/java/com/am/minor/fragments/TutorialFragment.java
+++ app/src/main/java/com/am/minor/fragments/TutorialFragment.java
@@ -1,5 +1,6 @@
 package com.am.minor.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +8,15 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.am.minor.BuyBooksActivity;
 import com.am.minor.R;
 
 public class TutorialFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tutorial, viewGroup, false);
+
+        Intent i  = new Intent(getActivity(), BuyBooksActivity.class);
+        startActivity(i);
 
         return view;
     }
---
+++ app/src/main/java/com/am/minor/fragments/stores/BooksWagon.java
@@ -0,0 +1,26 @@
+package com.am.minor.fragments.stores;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.am.minor.R;
+
+/**
+ * Created by Akshay on 12-07-2018.
+ */
+
+public class BooksWagon extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.store_frag_bookswagon, container, false);
+        return rootView;
+    }
+
+}
